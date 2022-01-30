d01b401057b6acded43ceb6168ba6d737a9b7759
Sanjeev <sanjeevcn@outlook.com>
2022-01-30 10:02:07 +0530

    added policy page and footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,12 @@
     <div class="container copyright">
       <span>© {{ new Date().getFullYear() }} {{ settings.site_name }}.</span>
       <br>
+      <small class="copylink">
+      <g-link to="/terms">Terms of use</g-link> |
+      <g-link to="/policy">Privacy policy</g-link> |
+      <g-link to="/contact">Contact us</g-link>
+    </small>
+      <br>
       <small v-html="settings.footer" />
     </div>
   </footer>
@@ -27,6 +33,10 @@ export default {
 .copyright {
   margin: 0 auto;
   text-align: center;
+}
+.copylink {
+  text-decoration: none;
+  line-height: 2rem;
 }
 
 small {
